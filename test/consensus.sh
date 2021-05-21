927f39e7a324b6ead454068ace677c154fd5821c
Marisa Lim <marisalim@users.noreply.github.com>
2021-05-21 08:48:40 -0700

    update primer name

--- test/consensus.sh
+++ test/consensus.sh
@@ -1,4 +1,4 @@
 for file in *.fastq; do
   bn=`basename $file .fastq`
-  NGSpeciesID --ont --consensus --sample_size 500 --m 800 --s 100 --medaka --primer_file Supplementary_File4_primer.txt --fastq $file --outfolder ${bn}
+  NGSpeciesID --ont --consensus --sample_size 500 --m 800 --s 100 --medaka --primer_file primers.txt --fastq $file --outfolder ${bn}
 done
